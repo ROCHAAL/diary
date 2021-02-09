cf483e9c3f5e16ca4cf557f13c08298a3e0dcaaf
Patricia <prmonday@gmail.com>
2021-02-09 09:17:32 +0000

    find_entries method

--- lib/daily_diary.rb
+++ lib/daily_diary.rb
@@ -1,13 +1,14 @@
 
 class Diary
-attr_reader :entries
+attr_reader :entries, :date, :body
 
 def initialize
   @entries = []
 end
 
     def find_entries(input_body)
-      @entries.select { |entry| entry == input_body}
+
+      @entries.select { |entry|  entry.body == input_body }
     end
 
   def add_entry(new_entry)
@@ -19,6 +20,7 @@ end
 
 class Entry
 
+attr_reader :date, :body
 def initialize(date, body)
 
 @date = date
